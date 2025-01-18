eb2503e7c8fbb78e1b2852e9a71c53e715a4864e
JaspeTn05 <jaspertielmann@web.de>
2025-01-18 19:36:54 +0100

    new statistics

--- analysis.R
+++ analysis.R
@@ -1,5 +1,5 @@
 # Workspace
-# setwd("./desktop/studium/semester_I/DH/dh-tagesschau-analysis")
+#setwd("./desktop/studium/semester_I/DH/dh-tagesschau-analysis")
 
 # Library Import Statement
 library(dplyr)
@@ -32,8 +32,8 @@ df <- dat %>%
     videoDuration, videoPlayCount, videoUrl, videoViewCount
   )
 
-# Zusammenfassung dynamische Reihen in eine Reihe mit Vectoren 
-
+# Zusammenfassung aller dynamischen Reihen taggedUsers.0 etc.
+# Funktion zur Zusammenfassung als kommagetrennte Liste (als String)
 df_summarize <- function(strings) {
   result <- c()  
   for (item in strings) {
@@ -57,10 +57,24 @@ df <- df %>%
       c(hashtags.0, hashtags.1, hashtags.2, hashtags.3, hashtags.4, 
         hashtags.5, hashtags.6, hashtags.7, hashtags.8, hashtags.9, 
         hashtags.10, hashtags.11)
-    )
+    ),
+caption_length = nchar(caption) # caption länge berechnung
   ) %>%
   ungroup()
 
+# Zusammengefasste Reihen rausgefiltert und fertig gereinigtes Datenset überschreiben
 df <- df %>%
   select(-starts_with("taggedUsers"), -starts_with("mentions"), -starts_with("hashtags"))
+
+## Vorbereitung für Topic Modeling - Deskreptive Statistik zur Caption (Laenge)
+
+avg_caption_length <- round(mean(df$caption_length),2)
+min_caption_length <- round(min(df$caption_length),2)
+max_caption_length <- round(max(df$caption_length),2)
+print(paste("Durchschnittliche Zeichen in Posting Caption:", avg_caption_length))
+print(paste("Maximale Anzahl an Zeichen in Posting Caption:", max_caption_length))
+print(paste("Minimale Anzahl an Zeichen in Posting Caption:", min_caption_length))
+
+
+
 
